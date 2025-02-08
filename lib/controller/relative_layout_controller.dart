7817ecd2de9aaac336cc0613ef7bd1e365696ac6
asmy8h <sinhaasmit99@gmail.com>
2025-02-08 22:23:57 +0530

    removed unused code

--- lib/controller/relative_layout_controller.dart
+++ lib/controller/relative_layout_controller.dart
@@ -1,16 +1,3 @@
 import 'package:flutter/material.dart';
 
-import '../models/button_relative_layout_model.dart';
-
-class RelativeLayoutController extends ChangeNotifier {
-  final List<ButtonRelativeLayoutModel> _buttons = [
-    ButtonRelativeLayoutModel(
-        buttonName: 'Button 1', alignment: Alignment.topLeft,),
-    ButtonRelativeLayoutModel(
-        buttonName: 'Button 2', alignment: Alignment.topRight),
-    ButtonRelativeLayoutModel(
-        buttonName: 'Button 3', alignment: Alignment.bottomLeft),
-    ButtonRelativeLayoutModel(
-        buttonName: 'Button 3', alignment: Alignment.bottomRight),
-  ];
-}
+class RelativeLayoutController extends ChangeNotifier {}
